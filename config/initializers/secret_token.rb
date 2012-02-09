49108f8c2974d3c53fb2e8d5b5c11014e6dc7d38
Hartaarn Saini <hartaarn@gmail.com>
2012-02-09 17:05:14 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyCommentsApp::Application.config.secret_token = '12cf23763e7385e599f11c29e5b6d49f7e958f67ff5346b3db7f63a29358706a1ff03cf19207cdd90d49b7bcc7f0620632c819efbe6f660e61d2aed5a6680aad'
